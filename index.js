2dd6dc465ab21a3e6dc526e4e61de5cf843c0c38
Alvaro <adholstein@gmail.com>
2018-12-24 10:12:28 +0000

    home page atualizada

--- index.js
+++ index.js
@@ -59,6 +59,46 @@ function sendEmail() {
     }
 }
 
+function selectYear() {
+    let edicoes = document.getElementsByClassName('button')
+    for (let i = 0; i < edicoes.length; i++) {
+        edicoes[i].addEventListener('click', event => {
+            let anoEscolhido = event.target.innerHTML
+
+            localStorage.setItem('ano', JSON.stringify(anoEscolhido))
+        })
+    }
+}
+function edition() {
+    let edition2017 = [{
+        nome: 'NeonSwitch',
+        descricaoJogo: 'É um jogo rápido com feel de arcade, situado num ambiente neon futurístico que envolve dois jogadores numa experiência 2D onde com o poder de alterar a gravidade dos objetos temos que derrotar o nosso oponente.',
+        nomeEquipa: "Games's Oven",
+        elementos: [{
+            nome: 'Beatriz Abreu'
+        },
+        {
+            nome: 'José Ribeiro'
+        },
+        {
+            nome: 'Igor Lima'
+        },
+        {
+            nome: 'Diogo Pereira'
+        },
+        {
+            nome: 'Pedro Fernandes'
+        }],
+        images: ['img1', 'img2', 'img3']
+    }]
+
+    if (document.getElementById('containerEdicoesAnteriores') != undefined) {
+        let ano = JSON.parse(localStorage.getItem('ano'))
+
+        document.getElementById('tituloEA').innerHTML = 'Edição de ' + ano
+    }
+}
+
 window.onload = function () {
     window.addEventListener('keypress', event => {
         if (event.key == '?') {
@@ -74,5 +114,10 @@ window.onload = function () {
 
     //Mandar E-mail para adholstein@gmail.com, por agora está a mandar para o meu mail
     sendEmail()
+
+    //Guardar em localStorage o ano da edição que o user quer ver
+    selectYear()
+    //Caso esteja na página Edições Anteriores
+    edition()
 }
 
