538019b515c74d74ea218630c984cbed1469ac32
Alvaro <adholstein@gmail.com>
2018-12-24 12:35:14 +0000

    navegablidade quase pronta

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ function ActivePage() {
             break;
     }
     // navlinks[2].className = 'nav-item active'
-    console.log(indexPage)
-    console.log(winLocation)
+    // console.log(indexPage)
+    // console.log(winLocation)
 
     for (let i = 0; i < navlinks.length; i++) {
         if (i == indexPage) {
             navlinks[i].className += ' active'
-            console.log(navlinks[i].className)
+            // console.log(navlinks[i].className)
         }
     }
 }
@@ -70,34 +70,119 @@ function selectYear() {
     }
 }
 function edition() {
-    let edition2017 = [{
-        nome: 'NeonSwitch',
-        descricaoJogo: 'É um jogo rápido com feel de arcade, situado num ambiente neon futurístico que envolve dois jogadores numa experiência 2D onde com o poder de alterar a gravidade dos objetos temos que derrotar o nosso oponente.',
-        nomeEquipa: "Games's Oven",
-        elementos: [{
-            nome: 'Beatriz Abreu'
-        },
-        {
-            nome: 'José Ribeiro'
-        },
-        {
-            nome: 'Igor Lima'
-        },
-        {
-            nome: 'Diogo Pereira'
-        },
-        {
-            nome: 'Pedro Fernandes'
-        }],
-        images: ['img1', 'img2', 'img3']
-    }]
-
     if (document.getElementById('containerEdicoesAnteriores') != undefined) {
+
+        let edition2017 = [{
+            nome: 'NeonSwitch',
+            descricaoJogo: 'É um jogo rápido com feel de arcade, situado num ambiente neon futurístico que envolve dois jogadores numa experiência 2D onde com o poder de alterar a gravidade dos objetos temos que derrotar o nosso oponente.',
+            nomeEquipa: "Games's Oven",
+            elementos: [{
+                nome: 'Beatriz Abreu'
+            },
+            {
+                nome: 'José Ribeiro'
+            },
+            {
+                nome: 'Igor Lima'
+            },
+            {
+                nome: 'Diogo Pereira'
+            },
+            {
+                nome: 'Pedro Fernandes'
+            }],
+            images: ['img1', 'img2', 'img3']
+        }]
+
+
         let ano = JSON.parse(localStorage.getItem('ano'))
 
         document.getElementById('tituloEA').innerHTML = 'Edição de ' + ano
     }
 }
+
+
+function alterarNumElementos() {
+
+    if (document.getElementById('containerInscricoes') != undefined) {
+
+        if (localStorage.getItem('novaEquipa')) {
+            let novaEquipa = JSON.parse(localStorage.getItem('novaEquipa'))
+            document.getElementById('nomeEquipa').value = novaEquipa.nomeEquipa
+            document.getElementById('nElementos').value = novaEquipa.nElementos
+            console.log(typeof novaEquipa.nElementos)
+            inserirCampos(Number(novaEquipa.nElementos))
+        }
+
+        document.getElementById('formEquipa').addEventListener('submit', event => {
+            event.preventDefault()
+        })
+
+        document.getElementById('nElementos').addEventListener('input', event => {
+            inserirCampos(event.target.value)
+        })
+
+        let elementosInscrever = document.getElementById('containerElementos')
+        elementosInscrever.innerHTML = ""
+        localStorage.removeItem('novaEquipa')
+    }
+}
+function inserirCampos(nCampos) {
+    let divPai = document.getElementById('containerElementos')
+    divPai.innerHTML = ""
+    if (nCampos > 5) nCampos = 5
+    if (nCampos == 0 || nCampos == undefined) nCampos = 1 //Não sei se isto é boa ideia aqui.
+
+    for (let i = 0; i < nCampos; i++) {
+
+        //Criar a "estrutura" dos "Elementos"
+        let article = document.createElement('article')
+        let nomeLabel = document.createElement('label')
+        let nome = document.createElement('input')
+
+        //
+        nome.id = "el" + i
+        nome.className = "form-control"
+        nome.setAttribute('required', '')
+        nomeLabel.innerHTML = "Nome:"
+        nomeLabel.setAttribute('for', nome.id)
+
+        //
+        article.appendChild(nomeLabel)
+        article.appendChild(nome)
+
+        //
+        divPai.appendChild(article)
+    }
+}
+
+function getTeamNameHome() {
+    if (document.getElementById('criarEquipaHome') != undefined) {
+        document.getElementById('criarEquipaHome').addEventListener('submit', event => {
+            event.preventDefault()
+            let form = {
+                nomeEquipa: '',
+                nElementos: ''
+            }
+
+            form.nomeEquipa = document.getElementById('nomeEquipaHome').value
+            form.nElementos = document.getElementById('nEquipaHome').value
+
+            console.log(form)
+
+            localStorage.setItem('novaEquipa', JSON.stringify(form));
+            window.location.href = "./inscricoes.html";
+        })
+    }
+}
+
+
+function clear(){
+    console.log('ola')
+    document.getElementById('nomeEquipa').value = ""
+    document.getElementById('nElementos').value = "1"
+}
+if(document.getElementById('clear')) document.getElementById('clear').addEventListener('click', clear)
 
 window.onload = function () {
     window.addEventListener('keypress', event => {
@@ -119,5 +204,14 @@ window.onload = function () {
     selectYear()
     //Caso esteja na página Edições Anteriores
     edition()
+
+    //Atualizar o 
+    alterarNumElementos()
+
+    //
+    getTeamNameHome()
+
+    
 }
+
 
